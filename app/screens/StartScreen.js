4e5dc6ad58b890dd56c3ba3557a38a1b8ccd2cc4
Irina Vlasceanu <irinav0402@gmail.com>
2019-05-21 21:22:40 +0300

    link custom fonts

--- app/screens/StartScreen.js
+++ app/screens/StartScreen.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import Layout from '../UI/Layout'
+import styles from '../styles/index.style';
 
 export default class StartScreen extends Component {
   render() {
     return (
       <Layout>
         <View>
-          <Text> This is the home screen with activities </Text>
+          <Text style={[styles.headline1, styles.textCenter]}>Headline 1</Text>
+          <Text style={[styles.headline2, styles.textCenter]}>Headline 2</Text>
+          <Text style={[styles.headline3, styles.textCenter]}>Headline 3</Text>
+          <Text style={[styles.title, styles.textCenter]}>Title</Text>
+          <Text style={[styles.subtitle, styles.textCenter]}>Subtitle</Text>
+          <Text style={[styles.bodyCopy]}>Body copy</Text>
+          <Text style={[styles.bodyCopySmall]}>Body copy small</Text>
         </View>
       </Layout>
     )
--- app/styles/index.style.js
+++ app/styles/index.style.js
@@ -11,3 +11,51 @@ export const colors = {
   gray: '#7D8184',
   softGray: '#425965',
 };
+
+export default StyleSheet.create({
+  textWhite: {
+    color: colors.white,
+  },
+  textBlack: {
+    color: colors.black,
+  },
+  textPrimaryLighter: {
+    color: colors.primaryLighter,
+  },
+  textGray: {
+    color: colors.gray,
+  },
+  headline1: {
+    fontSize: 30,
+    fontFamily: 'Work Sans SemiBold',
+  },
+  headline2: {
+    fontSize: 18,
+    fontFamily: 'Work Sans SemiBold',
+  },
+  headline3: {
+    fontSize: 14,
+    fontFamily: 'Work Sans SemiBold',
+  },
+  bodyCopy: {
+    fontSize: 14,
+    fontFamily: 'WorkSans-Light'
+  },
+  bodyCopySmall: {
+    fontSize: 12,
+    fontFamily: 'WorkSans-Light'
+  },
+  title: {
+    fontSize: 16,
+    fontFamily: 'Teko SemiBold',
+    textTransform: 'uppercase'
+  },
+  subtitle: {
+    fontSize: 12,
+    fontFamily: 'Teko Medium',
+    textTransform: 'uppercase'
+  },
+  textCenter: {
+    textAlign: 'center'
+  }
+})
